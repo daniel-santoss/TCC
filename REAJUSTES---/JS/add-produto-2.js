7c6b9846715dd298fe76156417b460feae37d25f
Daniel Vinicius <danielvinicius.santos7@gmail.com>
2024-03-13 21:45:42 -0300

    NEW NEW

---
+++ REAJUSTES---/JS/add-produto-2.js
@@ -0,0 +1,16 @@
+const adicionar = document.querySelector('.bi bi-plus-lg')
+const span = document.querySelector('.qtd')
+const subtrair = document.querySelector('.bi bi-dash-lg')
+
+
+function add(){
+    console.log('hdhod');
+}
+
+function sub(){
+    console.log('hdhsdddddddddddddddddod');
+}
+
+adicionar.addEventListener('click', add);
+subtrair.addEventListener('click', sub);
+
